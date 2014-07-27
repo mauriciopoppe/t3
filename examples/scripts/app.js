c290b34725dd65ebb1a8f98e1aefcefc6a6d406e
Mauricio Poppe <me@maurizzzio.com>
2014-07-27 01:22:30 -0400

    Using cdn.rawgit in the links

--- examples/scripts/app.js
+++ examples/scripts/app.js
@@ -1,8 +1,14 @@
 requirejs.config({
   baseUrl: 'scripts',
   paths: {
-    t3: '../dist/t3',
-    three: '../lib/three'
+    t3: [
+      '../dist/t3',
+      'https://cdn.rawgit.com/maurizzzio/t3/gh-pages/dist/t3'
+    ],
+    three: [
+      '../lib/three',
+      'https://cdn.rawgit.com/maurizzzio/t3/gh-pages/examples/lib/three.min'
+    ]
   }
 });
 
